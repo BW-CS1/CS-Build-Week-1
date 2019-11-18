ed1f0769f55153ab76c14d8433a211849d2abbd9
kevinsims1 <kevinsims1@outlook.com>
2019-11-18 17:34:20 -0600

    api up hello world response

--- adventure/urls.py
+++ adventure/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import api
 
 urlpatterns = [
+    url('hello_world', api.hello_world),
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
