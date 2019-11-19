8fc31c959803e63abb1f215ef3ff07010d68dc14
kevinsims1 <kevinsims1@outlook.com>
2019-11-18 23:00:27 -0600

    webpack babel and react downloaded

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf.urls import include
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
